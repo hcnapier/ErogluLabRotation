efc433024c83f482bb2f66504e23c61e0cfe0146
hcnapier <napier1@kenyon.edu>
2022-11-30 16:28:59 -0500

    Adds code to HepaCAM image analysis

--- code/scripts/image_analysis/20221122_HepaCAM.R
+++ code/scripts/image_analysis/20221122_HepaCAM.R
@@ -2,10 +2,15 @@
 # Hailey Napier
 # 20221122
 
-# Load packages
+# 0.0 Load packages ----
 
 
-# Load data
-hepa_36092.1_20221118_1 <- read.csv("../analysis_output/HepaCAM/36092-1/Colocalization_20221118_63x_36092-1_hepacam-488_mcherry-594")
-
-# 36092-1
+# 0.1 Load data ----
+## 36092-1 ----
+setwd("/Users/haileynapier/Work/Rotations/Rotation2_Eroglu/image_analysis/analysis_output/HepaCAM/36092-1")
+hepa_36092.1_20221118_1 <- read.csv("Colocalization_20221118_63x_36092-1_hepacam-488_mcherry-594_dapi.csv")
+hepa_36092.1_20221121_1 <- read.csv("Colocalization_20221121_36092-1_63x_hepacam-488_mcherry-594_dapi.csv")
+hepa_36092.1_20221121_2 <- read.csv("Colocalization_20221121_63x_36092-1_hepacam-488_mcherry-594_dapi_2.csv")
+hepa_36092.1_20221121_4 <- read.csv("Colocalization_20221121_63x_36092-1_hepacam-488_mcherry-594_dapi_4.csv")
+hepa_36092.1_20221121_3 <- read.csv("Colocalization_20221121_6xx_36092-1_hepacam-488_mcherry-594_dapi_3.csv")
+## 34872-3 ----
