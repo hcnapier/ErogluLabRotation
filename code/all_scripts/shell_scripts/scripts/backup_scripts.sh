0708cf8a74e08ff61877536c0a03e11b6fd66929
Hailey Napier <napier1@kenyon.edu>
2022-12-15 09:47:42 -0500

    Delete backup_scripts.sh

--- code/all_scripts/shell_scripts/scripts/backup_scripts.sh
+++
@@ -1,5 +0,0 @@
-# Hailey Napier
-# Script to back up shell scripts on local computer
-# December 1, 2022
-
-scp hcn4@dcc-login.oit.duke.edu:/work/hcn4/nlgn2_mintchip_wd/scripts /Users/haileynapier/Work/Rotations/Rotation2_Eroglu/code/scripts/shell_scripts 
